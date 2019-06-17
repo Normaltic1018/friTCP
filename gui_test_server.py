60921f3a22ee8dbbf8d80dd8d666f9ec7a7dd059
ikeeby <kby88power@naver.com>
2019-06-18 00:27:19 +0900

    get_cmd Thread_Recv

---
+++ gui_test_server.py
@@ -0,0 +1,24 @@
+import socket
+import os
+import threading
+
+def msg_recv(sock):
+    while(True):
+        print(sock.recv(65535).decode())
+
+#wait client
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('127.0.0.1', 12345))
+server_socket.listen(0)
+client_socket, addr = server_socket.accept()
+
+my_thread = threading.Thread(target=msg_recv, args=(client_socket,))
+my_thread.start()
+
+while(True):
+    msg = input(": ")
+    client_socket.send(msg.encode())
+
+
+client_socket.close()
--- tcp_socket.py
+++ tcp_socket.py
@@ -3,24 +3,22 @@ import time
 
 class TCP_SOCKET:
     def __init__(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind(('127.0.0.1', 12345))
-        self.server_socket.listen(0)
-        self.client_socket, addr = self.server_socket.accept()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect(('127.0.0.1', 12345))
         
     def send(self,data):
         try:
             print("send: "+data)
-            self.client_socket.send(data.encode())
+            self.sock.send(data.encode())
             
             # sleep 안주면 한번에 두번 보내는 경우가 생김.. 동기화를 위해 필수!
             time.sleep(0.2)
         except Exception as e:
-            self.client_socket.send(str(e).encode())
+            self.sock.send(str(e).encode())
 
     def recv(self):
         try:
-            data = self.client_socket.recv(65535).decode()
+            data = self.sock.recv(65535).decode()
             print("recv: "+data)
             return data
         except Exception as e:
