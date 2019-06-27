4847aece99d403e605b1ac2e98c10b370780e054
Normaltic1018 <normaltic1018@gmail.com>
2019-06-27 16:31:33 +0900

    [+] cmd_send_out,buff_send_out

--- tcp_proxy_interface/gui_interface.py
+++ tcp_proxy_interface/gui_interface.py
@@ -3,26 +3,17 @@
 from tcp_proxy_core.tcp_proxy_config import *
 from tcp_socket import *
 
-console_mode = False
+console_mode = True
 
 if(console_mode == False):
 	cmd_sock = TCP_SOCKET(12345)
 	proxy_sock = TCP_SOCKET(12344)
 
 def print_info():
-	if(console_mode):
-		print('{"type":"boot","data":{"version":"%s","settings":"%s","commands":"%s"}}' % (version, str(settings), str(commands)))
-	else:
-                #print("1version:{}".format(version))
-		#cmd_sock.send("version:{}".format(version))
-
-		#print("2[SETTING]:"+str(settings))
-		#cmd_sock.send("[SETTING]:"+str(settings))
-
-		#print("3[SETTING]:"+str(commands))
-		#cmd_sock.send("[SETTING]:"+str(commands))
-                cmd_sock.send('{"type":"boot","data":{"service":"init","res":"success","message":"core init success"}}')
-
+	send_data = '{"type":"boot","data":{"service":"init","res":"success","message":"core init success"}}'
+	cmd_send_out(send_data)
+	
+	#print('{"type":"boot","data":{"version":"%s","settings":"%s","commands":"%s"}}' % (version, str(settings), str(commands)))
                 
 def get_cmd():
 	if(console_mode):
@@ -51,7 +42,7 @@ def print_response(message):
 
 def print_current_settings():
 	send_data = '{"type":"cmd","data":{"res":"success","message":"%s"}}' % ("[SETTING]:"+str(settings))
-	
+
 	if(console_mode):
 		print("print_current_settings: "+send_data)
 	else:
@@ -75,41 +66,33 @@ def input_data():
 	return res
 	
 def print_js_response(message,proxy_info,hex_data):
-	if(console_mode):
-		if(message.startswith("[PROXY]")):
-			send_data = '{"type":"frida","data":{"service":"proxy","res":"success","message":{"INTERCEPT":"%s","IP":"%s","PORT":"%s","hex_dump":"%s"}}}' % (proxy_info[0],proxy_info[1],proxy_info[2],str(hex_data))
-			print(send_data)
-		elif(message.startswith("[HOOK_INFO]")):
-			send_data = '{"type":"frida","data":{"service":"hook_info","res":"success","message":{"PID":"%s","MODULE":"%s","FUNCTION":"%s","ADDRESS":"%s"}}}' % (proxy_info[0],proxy_info[1],proxy_info[2],proxy_info[3])
-			print(send_data)		
-		elif(message.startswith("[HEXDUMP]")):
-			send_data = '{"type":"frida","data":{"service":"hexdump","res":"success","message":{"hex_dump":"%s"}}}' % (str(hex_data))
-			print(send_data)
-		elif(message.startswith("[frida_error]")):
-			send_data = '{"type":"frida","data":{"service":"frida_error","res":"fail","message":{"error":"%s"}}}' % proxy_info
-			print(send_data)
-		elif(message.startswith("[frida_response]")):
-			send_data = '{"type":"frida","data":{"service":"frida_response","res":"?","message":{"proxy_info":"%s"}}}' % proxy_info
-			print(send_data)		
-		else:
-			send_data = '{"type":"frida","data":{"service":"else","res":"success","message":{"data":"%s"}}}' % (str(hex_data))
-			print(send_data)
+
+	// cmd_send_out과 buf_send_out은 추후 프로토콜 정하면서 구분할 것.
+	if(message.startswith("[PROXY]")):
+		send_data = '{"type":"frida","data":{"service":"proxy","res":"success","message":{"INTERCEPT":"%s","IP":"%s","PORT":"%s","hex_dump":"%s"}}}' % (proxy_info[0],proxy_info[1],proxy_info[2],str(hex_data))
+		buff_send_out(send_data)
+	elif(message.startswith("[HOOK_INFO]")):
+		send_data = '{"type":"frida","data":{"service":"hook_info","res":"success","message":{"PID":"%s","MODULE":"%s","FUNCTION":"%s","ADDRESS":"%s"}}}' % (proxy_info[0],proxy_info[1],proxy_info[2],proxy_info[3])
+		buff_send_out(send_data)		
+	elif(message.startswith("[frida_error]")):
+		send_data = '{"type":"frida","data":{"service":"frida_error","res":"fail","message":{"error":"%s"}}}' % proxy_info
+		buff_send_out(send_data)
+	elif(message.startswith("[frida_response]")):
+		send_data = '{"type":"frida","data":{"service":"frida_response","res":"?","message":{"proxy_info":"%s"}}}' % proxy_info
+		buff_send_out(send_data)		
 	else:
-		if(message.startswith("[PROXY]")):
-			send_data = '{"type":"frida","data":{"service":"proxy","res":"success","message":{"INTERCEPT":"%s","IP":"%s","PORT":"%s","hex_dump":"%s"}}}' % (proxy_info[0],proxy_info[1],proxy_info[2],str(hex_data))
-			proxy_sock.send(send_data)
-		elif(message.startswith("[HOOK_INFO]")):
-			send_data = '{"type":"frida","data":{"service":"hook_info","res":"success","message":{"PID":"%s","MODULE":"%s","FUNCTION":"%s","ADDRESS":"%s"}}}' % (proxy_info[0],proxy_info[1],proxy_info[2],proxy_info[3])
-			proxy_sock.send(send_data)	
-		elif(message.startswith("[HEXDUMP]")):
-			send_data = '{"type":"frida","data":{"service":"hexdump","res":"success","message":{"hex_dump":"%s"}}}' % (str(hex_data))
-			proxy_sock.send(send_data)
-		elif(message.startswith("[frida_error]")):
-			send_data = '{"type":"frida","data":{"service":"frida_error","res":"fail","message":{"error":"%s"}}}' % proxy_info
-			proxy_sock.send(send_data)
-		elif(message.startswith("[frida_response]")):
-			send_data = '{"type":"frida","data":{"service":"frida_response","res":"?","message":{"proxy_info":"%s"}}}' % proxy_info
-			proxy_sock.send(send_data)
-		else:
-			send_data = '{"type":"frida","data":{"service":"else","res":"success","message":{"data":"%s"}}}' % (str(hex_data))
-			proxy_sock.send(send_data)
+		send_data = '{"type":"frida","data":{"service":"else","res":"success","message":{"data":"%s"}}}' % (str(hex_data))
+		buff_send_out(send_data)
+
+
+def cmd_send_out(send_data):
+	if(console_mode):
+		print(send_data)
+	else:
+		cmd_sock.send(send_data)
+		
+def buff_send_out(send_data):
+	if(console_mode):
+		print(send_data)
+	else:
+		proxy_sock.send(send_data)
