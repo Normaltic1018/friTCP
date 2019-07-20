9016afcc2a4aadee2c8c5cba73c16abeb9037353
ikeeby <kby88power@naver.com>
2019-07-20 15:46:56 +0900

    [#] ssl

--- Cataclysm_Version/friTCP/ikeeby_socket_test/client_ssl.py
+++ Cataclysm_Version/friTCP/ikeeby_socket_test/client_ssl.py
@@ -1,18 +1,20 @@
 import socket
 import os
 import time
+import ssl
 
 print(os.getpid())
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('127.0.0.1', 12346))
+wrappedSocket = ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_TLSv1)
+wrappedSocket.connect(('127.0.0.1', 12346))
 
 while(True):
 	msg = "Hello Server!1234567890123456789012345678901234567890".encode()
-	sock.send(msg)
-	data = sock.recv(65535)
+	wrappedSocket.send(msg)
+	data = wrappedSocket.recv(65535)
 	
 	print(os.getpid())
 	print(data.decode())
 	time.sleep(1)
 
-sock.close()
+wrappedSocket.close()
---
+++ Cataclysm_Version/friTCP/ikeeby_socket_test/server_ssl.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+import ssl
+
+def msg_recv(sock):
+   while(True):
+      msg = "Welcome Client!".encode()
+      sock.send(msg)
+      data = sock.recv(65535).decode()
+      print(data)
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain('server.pem', 'server.key')
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:    
+	sock.bind(('127.0.0.1', 12346))
+	sock.listen(10)
+	with context.wrap_socket(sock, server_side=True) as ssock:
+		while(True):
+		   client_socket, addr = ssock.accept()
+		   print(client_socket)
+		   my_thread = threading.Thread(target=msg_recv, args=(client_socket,))
+		   my_thread.start()
+		
+    
+client_socket.close()
